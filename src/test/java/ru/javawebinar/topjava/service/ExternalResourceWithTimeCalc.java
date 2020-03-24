ba3f6186efccbdcbdf13fcd07231859b7b57aaa1
Beloussov Veniamin <venbelx@gmail.com>
2020-03-24 23:56:09 +0600

    Small clean for ExternalResource

--- src/test/java/ru/javawebinar/topjava/service/ExternalResourceWithTimeCalc.java
+++ src/test/java/ru/javawebinar/topjava/service/ExternalResourceWithTimeCalc.java
@@ -23,31 +23,10 @@ class ExternalResourceWithTimeCalc {
             resultSet.forEach(item -> logger.info(item.toString()));
             super.after();
         }
-
-        @Override
-        public Statement apply(Statement base, Description description) {
-            return new Statement() {
-                @Override
-                public void evaluate() throws Throwable {
-                    long startMills = System.currentTimeMillis();
-                    base.evaluate();
-                    long workingTime = System.currentTimeMillis() - startMills;
-                    TestResult testResult = new TestResult(description.getMethodName(), workingTime);
-                    logger.info(testResult.toString());
-                    resultSet.add(testResult);
-                }
-            };
-        }
     };
 
 
     ExternalResource externalResourceForMethod = new ExternalResource() {
-        @Override
-        protected void after() {
-            resultSet.forEach(item -> logger.info(item.toString()));
-            super.after();
-        }
-
         @Override
         public Statement apply(Statement base, Description description) {
             return new Statement() {
